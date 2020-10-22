fbb4c6aea5b5f516777e7223ebde4a44186fedd7
Santhosh Arockiaxavier <36897940+Santoz98@users.noreply.github.com>
2020-10-22 22:18:20 +0530

    Add files via upload

---
+++ precache-manifest.a236a2003bb15853dac70b24d6d20962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb61f27982f7c5526313a27ae92c2b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08ae973fc056af1f2b11",
+    "url": "/static/css/main.359d7092.chunk.css"
+  },
+  {
+    "revision": "1a289108f09930ec0292",
+    "url": "/static/js/2.94f3a0a0.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.94f3a0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ae973fc056af1f2b11",
+    "url": "/static/js/main.22aa27aa.chunk.js"
+  },
+  {
+    "revision": "df7d820ded1d4db0ff91",
+    "url": "/static/js/runtime-main.410ddc6b.js"
+  }
+]);
